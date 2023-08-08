61ba6cab8d41bc07236b5c197b8107c1052594ca
marcelojjunior <dbvmarcelo13@gmail.com>
2023-08-07 21:45:40 -0300

    add create and delete item

--- src/components/Item.tsx
+++ src/components/Item.tsx
@@ -1,26 +1,70 @@
 import { Trash } from "phosphor-react";
+import { useState } from "react";
 
 interface ItemProps {
     id: number;
-    title: string;
+    content: string;
     done: boolean;
+    onDeleteItem: (id: number) => void;
+    onToggleItem: (id: number) => void;
 }
 
-export function Item({ id, title, done }: ItemProps) {
+export function Item({ id, content, done, onDeleteItem, onToggleItem }: ItemProps) {
+    const [openModalDelete, setOpenModalDelete] = useState(false);
+
+    function handleDeleteComment(){
+        onDeleteItem(id);
+        setOpenModalDelete(false);
+    }
+
     return (
-        <div className="flex gap-2 justify-between bg-zinc-700 rounded-lg p-3 mb-3">
-            <div className="flex gap-2">
-                <label className="custom-checkbox text-base text-white">
-                    {title}
-                    <input type="checkbox"  checked={done} data-id={id}/>
-                    <span className="checkmark"></span>
-                </label>
+        <>
+            <div className="flex gap-2 justify-between bg-zinc-700 rounded-lg p-3 mb-3">
+                <div className="flex gap-2">
+                    <label className={`${done ? 'line-through text-zinc-400' : 'text-white'} custom-checkbox text-base`}>
+                        {content}
+                        <input
+                            type="checkbox"
+                            checked={done}
+                            data-id={id}
+                            onChange={() => onToggleItem(id)}
+                        />
+                        <span className="checkmark"></span>
+                    </label>
+                </div>
+                <button
+                    onClick={() => setOpenModalDelete(true)}
+                    className="text-white hover:text-red-600"
+                >
+                    <Trash size={16} />
+                </button>
             </div>
-            <button
-                className="text-white hover:text-red-600"
+            <div
+                className={`${openModalDelete ? 'block' : 'hidden'} bg-gray-800 bg-opacity-50 backdrop-blur z-40 fixed w-full left-0 top-0 h-screen flex justify-center items-center`}
             >
-                <Trash size={16} />
-            </button>
-        </div>
+                <div className="bg-zinc-700 w-max rounded-lg p-6 flex flex-col gap-6 justify-center items-center">
+                    <h1 className="font-bold text-2xl text-white">
+                        Excluir Item
+                    </h1>
+                    <p className="text-white text-base">
+                        Você tem certeza que deseja excluir esse item?
+                    </p>
+                    <div className="flex gap-4">
+                        <button
+                            onClick={() => setOpenModalDelete(false)}
+                            className="text-white hover:opacity-90 rounded-lg"
+                        >
+                            Cancelar
+                        </button>
+                        <button
+                            onClick={handleDeleteComment}
+                            className="text-red-600 bg-zinc-800 py-2 px-6 rounded-lg hover:opacity-90"
+                        >
+                            Sim, excluir
+                        </button>
+                    </div>
+                </div>
+            </div>
+        </>
     )
 }
--- src/components/List.tsx
+++ src/components/List.tsx
@@ -1,47 +1,80 @@
 import { PlusCircle } from "phosphor-react";
 import { Item } from "./Item";
 import { EmptyList } from "./EmptyList";
+import { ChangeEvent, FormEvent, useState } from "react";
 
-const items = [
-    {
-        id: 1,
-        title: "Estudar React",
-        done: true,
-    },
-    {
-        id: 2,
-        title: "Estudar JavaScript",
-        done: false,
-    },
-    {
-        id: 3,
-        title: "Estudar Typescript",
-        done: true,
-    }
-]
+interface ItemContent {
+    id: number;
+    content: string;
+    done: boolean;
+}
 
 export function List() {
+    const [items, setItems] = useState<ItemContent[]>([]);
+    const [newItemContent, setNewItemContent] = useState('');
+
+    function handleNewItemChange(event: ChangeEvent<HTMLInputElement>) {
+        setNewItemContent(event.target.value);
+    }
+
+    function handleCreateNewItem(event: FormEvent) {
+        event.preventDefault()
+
+        const newItem: ItemContent = {
+            id: items.length + 1,
+            content: newItemContent,
+            done: false
+        };
+
+        setItems([...items, newItem]);
+        setNewItemContent('');
+    }
+
+    function deleteItem(itemToDelete: number) {
+        const itemsWhiteOutDeleteOne = items.filter(item => {
+            return item.id !== itemToDelete
+        })
+
+        setItems(itemsWhiteOutDeleteOne);
+    }
+
+    function handleToggle(id: number) {
+        const updatedItems = items.map((item) =>
+            item.id === id ? { ...item, done: !item.done } : item
+        );
+        setItems(updatedItems);
+    }
+
+
+
     return (
         <div className="flex flex-col -mt-14">
-            <div className="flex w-full justify-center gap-2">
+            <form
+                className="flex w-full justify-center gap-2"
+                onSubmit={handleCreateNewItem}
+            >
                 <input
                     className="w-5/6 bg-[#202024] shadow-xl rounded-lg p-3 text-gray-100 placeholder:text-gray-500"
                     type="text"
                     placeholder="Adicione uma nova tarefa"
+                    value={newItemContent}
+                    onChange={handleNewItemChange}
                 />
                 <button
+                    type="submit"
                     className="w-1/6 bg-green-700 hover:bg-green-800 shadow-xl rounded-lg text-white flex justify-center items-center gap-2"
                 >
                     Criar
                     <PlusCircle size={20} />
                 </button>
-            </div>
+            </form>
+
             <div className="mt-10 flex justify-between mb-5">
                 <div>
                     <p className="text-green-700 font-bold flex gap-2 text-sm">
                         Tarefas criadas
                         <span className="bg-gray-600 rounded-2xl px-3 text-gray-300">
-                            0
+                            {items.length}
                         </span>
                     </p>
                 </div>
@@ -57,7 +90,14 @@ export function List() {
             <div>
                 {items.length === 0 ? <EmptyList /> : (
                     items.map((item) => (
-                        <Item key={item.id} {...item} />
+                        <Item
+                            key={item.id}
+                            id={item.id}
+                            content={item.content}
+                            done={item.done}
+                            onToggleItem={handleToggle}
+                            onDeleteItem={deleteItem}
+                        />
                     ))
                 )}
 
